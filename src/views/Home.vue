ec6832b3c6c0812f634bc17adba26a5ed3c8172f
Adis Hadzic <adis7hadzic@gmail.com>
2021-01-08 19:30:38 +0100

    packagelock json...

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
-    <div>home</div>
+   <div class="progress">
+  <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
+</div>
 </template>
 
 <script>
