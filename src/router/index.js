b5a8ab2063b234a57319ba0cd0ac01a9be8e0887
Alma Smajic <53364813+almasmajic@users.noreply.github.com>
2021-01-10 23:13:41 +0100

    bug fix

--- src/router/index.js
+++ src/router/index.js
@@ -60,6 +60,22 @@ const routes = [
     name: 'MBeginner',
     component: () => import(/* webpackChunkName: "about" */ '../views/MBeginner.vue')
   },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Dashboard.vue')
+  },
+  {
+    path: '/chooseworkout',
+    name: 'ChooseWorkout',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/ChooseWorkout.vue')
+  },
 
 ]
 
