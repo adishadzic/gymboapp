838f576f233d76c215ebfd812065fc8b53a9a808
Alma Smajic <53364813+almasmajic@users.noreply.github.com>
2021-02-06 00:41:08 +0100

    edit on home and BMI

--- src/views/BMI.vue
+++ src/views/BMI.vue
@@ -1,50 +1,65 @@
 <template>
 <div class="app">
   <br>
-      <div class="row mt-5">
-        <div class="col-0 image">
+
+    <b-container fluid class="container mt-4">
+      <b-row class="d-flex inline-flex justify-content-start">
+        <b-col class="mr-4 ml-3">
           <img src="@/assets/avatar.jpg" alt="Avatar" class="avatar">
           <h3 class="mt-3">{{ store.currentUser }}</h3>
           <router-link to="/editprofile">
           <button type="button" class="btn btn-secondary">Edit Profile</button>
           </router-link>
-        </div>
-        <div class="ml-5"><router-link to="/dashboard/workouts" class="nav-link text-light active"><img src="@/assets/dumbbell.png" class="dumbbell-logo1 mr-1" height="25">Workouts</router-link>
+        </b-col>
 
-        <b-row class="my-1" >
-      <b-col sm="3">
-        <label :for="`type-Height`"> Height </label>
-      </b-col>
-      <b-col sm="9">
-        <b-form-input :id="`type-Height`" v-model="Height"></b-form-input>
-      </b-col>
-    </b-row>
-<b-row class="my-1" >
-      <b-col sm="3">
-        <label :for="`type-Weight`"> Weight </label>
-      </b-col>
-      <b-col sm="9">
-        <b-form-input :id="`type-Weight`" v-model="Weight"></b-form-input>
-      </b-col>
-    </b-row>
+        <b-col cols="8">
+          <b-row no-gutters>
+            <b-col>
+              <router-link to="/dashboard/workouts" class="nav-link text-light active"><img src="@/assets/dumbbell.png" class="mr-1" height="20">Workouts</router-link>
+            </b-col>
+        
+            <b-col>
+              <router-link to="/dashboard/myplan" class="nav-link text-light"><img src="@/assets/pencil.png" class="mr-1" height="20">My Plan</router-link>
+            </b-col>
 
-  <div>
-    <b-button class="mb-2" variant="primary" @click="bmiToast()">
-      Calculate
-    </b-button>
-    <b-toast id="example-toast" title="Results say that.." static no-auto-hide>
-      Your BMI index is {{bmi}}
-    </b-toast>
-  </div>
+            <b-col>
+              <router-link to="/dashboard/pics" class="nav-link text-light"><img src="@/assets/image.png" class="mr-1" height="20">Pics</router-link>
+            </b-col>
 
-        </div>
-        <div class="ml-5"><router-link to="/dashboard/myplan" class="nav-link text-light"><img src="@/assets/pencil.png" class="mr-1" height="20">My Plan</router-link>
-        </div>
-        <div class="ml-5"><router-link to="/dashboard/pics" class="nav-link text-light"><img src="@/assets/image.png" class="mr-1" height="20">Pics</router-link>
-        </div>
-        <div class="ml-5"><router-link to="/dashboard/BMI" class="nav-link text-light"><img src="@/assets/image.png" class="mr-1" height="20">BMI</router-link>
-        </div>
-      </div>
+            <b-col>
+              <router-link to="/dashboard/BMI" class="nav-link text-light"><img src="@/assets/bmi1.png" class="mr-1" height="20">BMI</router-link>
+            </b-col>
+          </b-row>
+            <b-row class="my-1 mt-3">
+              <b-col sm="3" class="mt-2">
+                  <label :for="`type-Height`"> Height </label>
+              </b-col>
+              <b-col sm="5">
+                  <b-form-input :id="`type-Height`" v-model="Height"></b-form-input>
+              </b-col>
+            </b-row>
+          <b-row class="my-1 mt-3">
+              <b-col sm="3" class="mt-2">
+                  <label :for="`type-Weight`"> Weight </label>
+              </b-col> 
+              <b-col sm="5">
+                  <b-form-input :id="`type-Weight`" v-model="Weight"></b-form-input>
+              </b-col>
+          </b-row>
+
+          <b-row>
+            <b-col sm="5" style="left: 25%">
+              <b-button class="mb-2 mt-2" variant="primary" @click="bmiToast()">
+                  Calculate
+              </b-button>
+              <b-toast id="example-toast" title="Results say that.." static no-auto-hide>
+                  Your BMI index is {{bmi}}
+              </b-toast>
+            </b-col>
+          </b-row>
+        </b-col>
+      </b-row>
+    </b-container>
 </div>
 
 </template>
@@ -78,50 +93,18 @@ export default {
       }
     }
   }
-  
-/* import TestCard from '@/components/TestCard.vue';
-
-let cards = [];
-
-cards = [
-  {
-    img: require('@/assets/gym.jpg'),
-    route: '/login',
-  },
-];
-
-export default {
-  name: 'Dashboard',
-  data: function() {
-    return {
-      cards,
-    };
-  },
-  components: {
-    TestCard,
-  }
-}; */
-</script>
+  </script>
 
 <style scoped>
+.container {
+  margin-left: -150px;
+}
 a.router-link-active{
   background: gray;
-}
-/* .nav-link:hover{
-  background-color: gray;
-}
-.nav-link:focus{
-  background-color: red;
-} */
-.image{
-  margin-left: -120px;
 }
 .avatar {
   width: 300px;
   height: 300px;
   border-radius: 50%;
-}
-.dumbbell-logo{
-  margin-top: -2px;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,18 @@
 <template>
-    <div class="intro">
-      <h1>How it works</h1>
-    <div class="container">
-      <div class="row">
-    <div class="col">
-      <h5>Introductory video</h5>
+  <div class="row">
+  <div class="col">
       <div class="embed-responsive embed-responsive-16by9">
         <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Mdj711U31yU" allowfullscreen></iframe>
       </div>
-    </div>
-    <div class="col">
-      <h5>Here goes a short description about our app + social media links</h5>
-      <br>
-    </div>
-  </div>
-  <div class="cta">
+        <div class="cta mt-3">
     <button type="button" class="btn btn-primary">Get started</button>
   </div>
-    </div>
-    </div>
+  </div>
+  <div class="col-5">
+    <h1 class="presenting-title">How Gymbo works</h1>
+    <img class="img-present-vid" src=@/assets/coach-home.png />
+  </div>
+</div>
 </template>
 
 <script>
@@ -26,10 +20,13 @@
 </script>
 
 <style>
-  .col{
-    margin-top: 20px;
-  }
-  .cta{
-    margin-top: 30px;
-  }
+.img-present-vid {
+  position: relative; /* can play with absolutes too */
+  left: 100px;
+}
+
+.presenting-title{
+  top: 600px;
+  text-align: right;
+}
 </style>
